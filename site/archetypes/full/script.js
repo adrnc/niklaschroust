7d46cb04f3f730ff1c66eb130ab0b1ee3a3f2729
adrianonym <49120352+adrianonym@users.noreply.github.com>
2020-04-08 15:59:33 +0200

    cleanup

--- site/archetypes/full/script.js
+++
@@ -1,6 +0,0 @@
-const MAIN = document.querySelector('main'); // recommended
-
-const div = document.createElement('p');
-div.className = 'javascript'
-div.appendChild(document.createTextNode('Inserted via JavaScript!'));
-MAIN.appendChild(div);
