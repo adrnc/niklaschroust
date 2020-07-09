f7fafa0b6496594ebe8052d30e380b853b3f1a2a
adrianonym <49120352+adrianonym@users.noreply.github.com>
2020-07-10 00:03:02 +0200

    hugo service worker and optimisation

---
+++ site/assets/js/sw.js
@@ -0,0 +1,17 @@
+// {{ cond hugo.IsProduction now "" }} hugo template
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		(async () => {
+			console.log(event.request);
+			const cache = await caches.match(event.request);
+			if (cache) return cache;
+
+			const response = await fetch(event.request);
+			if (response.ok && response.status == 200 && /^https?:\/\/[^\/]+\/(js|css)\//g.test(response.url)) {
+				(await caches.open('app')).put(event.request, response.clone());
+			}
+			return response;
+		})()
+	);
+});
