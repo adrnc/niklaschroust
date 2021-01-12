3e0e9b1d06474f82298e1465218a779f04460a84
adrianonym <49120352+adrianonym@users.noreply.github.com>
2021-01-12 22:41:31 +0100

    Update sw.js

--- site/assets/sw.js
+++ site/assets/sw.js
@@ -6,8 +6,8 @@ self.addEventListener('fetch', event => {
 			const cache = await caches.match(event.request);
 			if (cache) return cache;
 
-			const url = new URL(event.request);
 			const response = await fetch(event.request);
+			const url = new URL(response.url);
 			if (location.origin == url.origin && response.ok && response.status == 200 && /^\/(js|css)\//g.test(url.pathname)) {
 				(await caches.open('app')).put(event.request, response.clone());
 			}
